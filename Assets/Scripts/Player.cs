1cbaafe93aa3534523ab8a5a82568bca50c28c31
Kane-Kaje <jianu.daniel9531@gmail.com>
2024-04-09 08:09:57 +0300

    1st player changes
    using the new unity input manager I added a few scripts to implement player movement, jump is not yet implemented.
    I also tested making blocks transparent when the player is behind them

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,150 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.EventSystems;
+
+public class Player : MonoBehaviour
+{
+    public static Player Instance { get; private set; }
+
+
+/*    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs
+    {
+        public ClearCounter selectedCounter;
+    }*/
+
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
+
+
+    private bool isWalking;
+    private Vector3 lastInteractDirection;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There is more than one player instance");
+        }
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        //gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+/*    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        if (selectedCounter != null)
+        {
+            selectedCounter.Interact();
+        }
+    }*/
+
+    private void Update()
+    {
+        HandleMovement();
+      //HandleInteractions();
+    }
+
+    /*private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDirection != Vector3.zero)
+        {
+            lastInteractDirection = moveDirection;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDirection, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) //Has Clear Counter
+            {
+                //here is the interaction
+                if (clearCounter != selectedCounter)
+                {
+                    SetSelectedCounter(clearCounter);
+                }
+            }
+            else
+            {
+                SetSelectedCounter(null);
+            }
+        }
+        else
+        {
+            SetSelectedCounter(null);
+        }
+    }*/
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+
+        if (!canMove) //cannot move towards this direction
+        {
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
+
+            if (canMove) //can move only on the X
+            {
+                moveDirection = moveDirectionX;
+            }
+            else //if cannot move only on the X , we try Z
+            {
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
+                if (canMove) //can move only on the Z
+                {
+                    moveDirection = moveDirectionZ;
+                }
+                else
+                {
+                    //cannot move
+                }
+            }
+
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDirection * moveDistance;
+        }
+
+        isWalking = moveDirection != Vector3.zero;
+
+        float rotateSpeed = 13f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+    }
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+/*    private void SetSelectedCounter(ClearCounter selectedCounter)
+    {
+        this.selectedCounter = selectedCounter;
+
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs
+        {
+            selectedCounter = selectedCounter
+        });
+    }*/
+
+
+}
