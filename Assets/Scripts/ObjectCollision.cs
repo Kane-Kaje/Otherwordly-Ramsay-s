a11520f45ef5185d6d03c8f20d606cf20a4c1274
kingofchips <115977454+kingofchips@users.noreply.github.com>
2024-05-28 01:34:50 +0300

    Update object collission
    Now the items that the player collects will disappear when they touch them.

--- Assets/Scripts/ObjectCollision.cs
+++ Assets/Scripts/ObjectCollision.cs
@@ -4,72 +4,18 @@ using UnityEngine;
 
 public class ObjectCollision : MonoBehaviour
 {
+    public GameObject tomato;
+    public GameObject cabbage;
     void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.tag == "stove") {
-            print("You hit the stove!");
+        if (collision.gameObject.tag == "tomato")
+        {
+            tomato.gameObject.SetActive(false);
         }
 
-        if (collision.gameObject.tag == "kitchen_counter") {
-            print("You hit the kitchen counter!");
-        }
-
-        if (collision.gameObject.tag == "tomato") {
-            print("You hit the tomato!");
-        }
-
-        if (collision.gameObject.tag == "cabbage") {
-            print("You hit the cabbage!");
-        }
-
-        if (collision.gameObject.tag == "trash_can") {
-            print("You hit the trash can!");
-        }
-    }
-
-    void OnTriggerStay(Collider collision)
-    {
-        if (collision.gameObject.tag == "stove") {
-            print("You're still hitting the stove.");
-        }
-
-        if (collision.gameObject.tag == "kitchen_counter") {
-            print("You're still hitting the kitchen counter.");
-        }
-
-        if (collision.gameObject.tag == "tomato") {
-            print("You're still hitting the tomato.");
-        }
-
-        if (collision.gameObject.tag == "cabbage") {
-            print("You're still hitting the cabbage.");
-        }
-
-        if (collision.gameObject.tag == "trash_can") {
-            print("You're still hitting the trash can.");
-        }
-    }
-
-    void OnTriggerExit(Collider collision)
-    {
-        if (collision.gameObject.tag == "stove") {
-            print("You stop hitting the stove.");
-        }
-
-        if (collision.gameObject.tag == "kitchen_counter") {
-            print("You stop hitting the kitchen counter.");
-        }
-
-        if (collision.gameObject.tag == "tomato") {
-            print("You stop hitting the tomato.");
-        }
-
-        if (collision.gameObject.tag == "cabbage") {
-            print("You stop hitting the cabbage.");
-        }
-
-        if (collision.gameObject.tag == "trash_can") {
-            print("You stop hitting the trash can.");
+        if (collision.gameObject.tag == "cabbage")
+        {
+            cabbage.gameObject.SetActive(false);
         }
     }
 }
